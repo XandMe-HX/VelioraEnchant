0013158f7946717dbef26ea60977a303a598ad86
XandMe Studio <kevindwinurhuda@gmail.com>
2026-09-01 17:42:02 +0700

    feat: add fishing rarity rolls and secret fragments

---
+++ src/main/java/id/velioragardens/enchant/FishingRarity.java
@@ -0,0 +1,15 @@
+package id.velioragardens.enchant;
+
+import net.kyori.adventure.text.format.NamedTextColor;
+import java.util.List;
+
+enum FishingRarity {
+    COMMON("common", NamedTextColor.WHITE, List.of("angler_luck", "deep_hook")),
+    RARE("rare", NamedTextColor.GREEN, List.of("double_catch", "fisherman_heal", "treasure_hook")),
+    EPIC("epic", NamedTextColor.LIGHT_PURPLE, List.of("auto_reel", "guardian_hook", "ocean_blessing", "river_spirit")),
+    LEGENDARY("legendary", NamedTextColor.GOLD, List.of("sunken_relic", "storm_angler", "mermaid_tears")),
+    MYTHIC("mythic", NamedTextColor.RED, List.of("abyssal_hook", "leviathan_line")),
+    SECRET("secret", NamedTextColor.DARK_PURPLE, List.of("veliora_secret"));
+    final String id; final NamedTextColor color; final List<String> enchantments;
+    FishingRarity(String id, NamedTextColor color, List<String> enchantments) { this.id=id; this.color=color; this.enchantments=enchantments; }
+}
